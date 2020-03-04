81870e3da65f6d7d50ceae20f218531e26646bb2
ThanhMinh <minh8631@gmail.com>
2020-03-04 14:30:24 +0700

    update

--- src/theme/pages/Following.js
+++ src/theme/pages/Following.js
@@ -1,114 +1,113 @@
 import React, {Component} from 'react';
-import {View, Text, Image, FlatList, ScrollView, TouchableOpacity, Alert, TextInput} from "react-native";
+import {View, Text, Image, FlatList, ScrollView, TouchableOpacity, Alert, TextInput,ActivityIndicator} from "react-native";
+import { ListItem, SearchBar } from 'react-native-elements';
 
-const items = [
-    {id: 1, ag: 'user 1', agm: 'full name 1', name: 'https://cdn.pixabay.com/photo/2015/02/09/20/03/koala-630117__340.jpg'},
-    {id: 2, ag: 'user 2', agm: 'full name 2', name: 'https://cdn.pixabay.com/photo/2019/07/25/01/38/kangaroo-4361467__340.jpg'},
-    {id: 3, ag: 'user 3', agm: 'full name 3', name: 'https://cdn.pixabay.com/photo/2019/07/25/01/38/kangaroo-4361467__340.jpg'},
-    {id: 4, ag: 'user 4', agm: 'full name 4', name: 'https://cdn.pixabay.com/photo/2019/07/25/01/38/kangaroo-4361467__340.jpg'},
-    {id: 5, ag: 'user 5', agm: 'full name 5', name: 'https://cdn.pixabay.com/photo/2019/07/25/01/38/kangaroo-4361467__340.jpg'},
-]
 
 class Following extends Component {
-
     constructor(props) {
         super(props);
+
         this.state = {
-            data: items,
-            tenbaihat: '',
-        }
-        this.componentDidMount_BaiH();
+            loading: false,
+            data: [],
+            error: null,
+        };
+
+        this.arrayholder = [];
     }
-    componentDidMount_BaiH() {
-        return fetch(
-            'https://nhacmp3vip.000webhostapp.com/api_appnhac/Baihat_Get.php'
-        )
-            .then(response => response.json())
-            .then(responseJson_baih => {
-                console.log(responseJson_baih);
+
+    componentDidMount() {
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        const url = `https://nhacmp3vip.000webhostapp.com/api_appnhac/Baihat_Get.php`;
+        this.setState({ loading: true });
+
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
                 this.setState({
-                    dataSource_BaiH: responseJson_baih
+                    data: res,
+                    error: res.error || null,
+                    loading: false,
                 });
+                this.arrayholder = res;
             })
             .catch(error => {
-                console.error(error);
+                this.setState({ error, loading: false });
             });
-    }
+    };
 
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: '86%',
+                    backgroundColor: '#CED0CE',
+                    marginLeft: '14%',
+                }}
+            />
+        );
+    };
 
+    searchFilterFunction = text => {
+        this.setState({
+            value: text,
+        });
+
+        const newData = this.arrayholder.filter(item => {
+            const itemData = `${item.TenBaiHat.toUpperCase() }`;
+            const textData = text.toUpperCase();
+
+            return itemData.indexOf(textData) > -1;
+        });
+        this.setState({
+            data: newData,
+        });
+    };
+
+    renderHeader = () => {
+        return (
+            <SearchBar
+                placeholder="Type Here..."
+                lightTheme
+                round
+                onChangeText={text => this.searchFilterFunction(text)}
+                autoCorrect={false}
+                value={this.state.value}
+            />
+        );
+    };
 
     render() {
-        return (
-            <View style={{flex:1}}>
-                <View style={{flexDirection:'row', borderWidth:1 , borderColor:'#cbcbcb', alignItems:'center'}}>
-                    <TextInput style={{fontSize:20, width:'80%'}}
-                               placeholder="Tìm kiếm"
-                               keyboardType='numeric'
-                               returnKeyType='next'
-                               autoCorrect={false}
-                               onChangeText={(tenbaihat) => this.setState({tenbaihat})}
-                               value={this.state.tenbaihat}
-                    />
-                    <TouchableOpacity style ={{width:'20%',height:50, borderWidth:1, borderColor:'#cbcbcb', justifyContent:'center', alignItems:'center'}}>
-                        <Image style={{
-                            width:30,
-                            height: 30,
-                            marginRight: 10,
-                            borderRadius: 10
-                        }}
-                               source={require('../../asset/iconsearch.png')}/>
-                    </TouchableOpacity>
-
+        if (this.state.loading) {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <ActivityIndicator />
                 </View>
-                <ScrollView>
-                    <View style={{flex: 1}}>
-                        <View>
-                            <FlatList
-                                data={this.state.dataSource_BaiH} renderItem={({item}) =>
-                                <TouchableOpacity onPress={()=>{}} style={{
-                                    marginLeft: 10,
-                                    marginRight: 10,
-                                    marginTop: 5,
-                                    marginBottom:5,
-                                    elevation: 3,
-                                    orderRadius: 10,
-                                    borderRadius: 10,
-                                    backgroundColor: '#f5f5f5'
-                                }}>
-                                    <View  style={{flexDirection: 'row', padding: 10}}>
-                                        <View style={{flex: 1}}>
-                                            <Image style={{
-                                                width: 50,
-                                                height: 50, borderRadius: 10
-                                            }}
-                                                   source={{uri: item.HinhBaiHat}}/>
-                                        </View>
-                                        <View style={{flex: 5, marginLeft: 15, justifyContent:'center'}}>
-                                            <Text style={{color: '#4b4b4b'}}>
-                                                {item.TenBaiHat}
-                                            </Text>
-                                            <Text style={{color: '#4b4b4b'}}>
-                                                {item.CaSi}
-                                            </Text>
-                                        </View>
-                                        <View style={{flex: 1, alignItems: 'flex-end', marginTop: 5}}>
-                                            <Image style={{
-                                                width: 40,
-                                                height: 40
-                                            }}
-                                                   source={require('../../asset/iconplay.png')}/>
-                                        </View>
-                                    </View>
-                                </TouchableOpacity>
-                            }/>
-                        </View>
-
-                    </View>
-                </ScrollView>
+            );
+        }
+        return (
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            leftAvatar={{ source: { uri: item.HinhBaiHat } }}
+                            title={`${item.TenBaiHat} `}
+                            subtitle={item.CaSi}
+                        />
+                    )}
+                    keyExtractor={item => item.email}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    ListHeaderComponent={this.renderHeader}
+                />
             </View>
-
         );
     }
 }
+
 
 export default Following;
